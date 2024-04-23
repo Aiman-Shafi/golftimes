bca75001a39e5ed1aa153b3557225b7120b86861
Aiman Shafi <shafiaiman88@gmail.com>
2024-04-23 21:58:29 +0600

    add: services on strapi & frontend

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="max-w-6xl text-center mx-auto py-40">
+      <h2 className="text-center text-xl text-indigo-400 uppercase">
+        loading...
+      </h2>
+    </div>
+  );
+}
